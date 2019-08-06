d5d0b4d3970e32b18bd08fa7f091ca6f8fb54264
Andrey <asanchez@intelligentsense.com>
2019-08-05 19:08:16 -0600

    [promises] - callbacks were substituted to promises

--- web-boilerplate-master/app/js/main.js
+++ web-boilerplate-master/app/js/main.js
@@ -1,12 +1,33 @@
 $(document).ready(function () {
-    var output = $("#output");
-    var template = $("#output-script").html();
+  var output = $("#output");
+  var template = $("#output-script").html();
+
+  function getPosts() {
+    return new Promise(function(resolve, reject) {
     var req = new XMLHttpRequest();
-    req.open('GET', 'https://api.myjson.com/bins/uptto', false);
-    req.send(null);
-    var data;
-    if (req.status == 200)
-      data = req.responseText;
-      html = Mustache.render(template, JSON.parse(data));
-      output.append(html);
+        req.open('GET', 'https://api.myjson.com/bins/uptto', false);
+        req.onload = function() {
+          if (req.status == 200) {
+            alert(req.response);
+            resolve(JSON.parse(req.response));
+          }
+          else {
+            reject();
+          }
+        };
+        req.send();
+    })
+}
+
+getPosts().then(function(data){
+  alert("it worked");
+  alert(data);
+   html = Mustache.render(template, data);
+   output.append(html);
+  }).catch(() => {
+    alert('Algo salió mal');
+  });
+
+
+
 });
