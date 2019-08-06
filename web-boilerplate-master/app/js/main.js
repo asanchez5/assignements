f5aef4c3833bfa8d231e1283e35f079967ccd971
Andrey <asanchez@intelligentsense.com>
2019-08-05 19:21:41 -0600

    [mustache] - The link was changed and images were added

--- web-boilerplate-master/app/js/main.js
+++ web-boilerplate-master/app/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var output = $("#output");
     var template = $("#output-script").html();
     var req = new XMLHttpRequest();
-    req.open('GET', 'https://api.myjson.com/bins/uptto', false);
+    req.open('GET', 'https://api.myjson.com/bins/1bpb69', false);
     req.send(null);
     var data;
     if (req.status == 200)
