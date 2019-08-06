60b296d0670e464da94e046f1e99b235afd97058
Andrey <asanchez@intelligentsense.com>
2019-08-06 11:02:40 -0600

    [Promises] - Search engine implemented

--- web-boilerplate-master/app/js/main.js
+++ web-boilerplate-master/app/js/main.js
@@ -18,13 +18,36 @@ $(document).ready(function () {
     })
 }
 
-getPosts().then(function(data){
-   html = Mustache.render(template, data);
-   output.append(html);
-  }).catch(() => {
-    console.log('Something went wrong');
-  });
+  getPosts().then(function(data){
+     html = Mustache.render(template, data);
+     output.append(html);
+    }).catch(() => {
+      console.log('Something went wrong');
+    });
 
+    function search() {
+        var input, set_elements, label_element, i, text_value;
+        input = document.getElementById("search_bar");
+        set_elements = document.getElementById("output").getElementsByClassName("flex-container__item");
+        for (i = 0; i < set_elements.length; i++) {
+            text_value = set_elements[i].getElementsByTagName("label")[0].textContent;
+            if (text_value.toUpperCase().indexOf(input.value.toUpperCase()) > -1) {
+                set_elements[i].style.display = "inline-block"
+            } else {
+                set_elements[i].style.display = 'none';
+            }
+        }
+    }
 
+    document.getElementById('search_bar').onkeypress = function(e){
+        if (!e) e = window.event;
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == '13'){
+          search();
+        }
+      }
 
+      document.getElementById("search_button").addEventListener("click", function(){
+        search();
+      });
 });
